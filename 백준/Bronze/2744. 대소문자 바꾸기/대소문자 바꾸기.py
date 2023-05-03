918e611b632b514197c516e6856894c24591e41c
Heejung Choi <71762328+hdddhdd@users.noreply.github.com>
2023-05-03 23:31:58 +0900

    Update 대소문자 바꾸기.py

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
@@ -1,4 +1,14 @@
 # part1 - 230503
 inputstr = input()
-outputstr = inputstr.swapcase()
-print(outputstr)
+# outputstr = inputstr.swapcase()
+# print(outputstr)
+answer = ""
+for c in inputstr:
+    if c.isupper():
+        answer += c.lower()
+        #print(answer)
+    else:
+        answer += c.upper()
+        #print(answer)
+
+print(answer)
